45ee6d8725572dc0740d87480856f2e7d9060984
Isabella Campos <isabellaszcamposs@gmail.com>
2023-11-23 10:32:26 -0300

    chore: create project

---
+++ src/main/java/fiap/micro/ods/OdsApplication.java
@@ -0,0 +1,13 @@
+package fiap.micro.ods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fiap/micro/ods/Ods2MicroApplicationTests.java
@@ -0,0 +1,13 @@
+package fiap.micro.ods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ods2MicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
